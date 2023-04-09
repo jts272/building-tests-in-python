b4af17395d195c3c5b252add82e1c26e2e1d5613
jts272 <jts272@outlook.com>
2023-04-09 01:13:58 +0100

    refactor: add setup and teardown instances
    Running the Python file will show the tests that are run and in which order. Any additional dots shown represent a unittest pass

--- test_student.py
+++ test_student.py
@@ -3,23 +3,35 @@ from student import Student
 
 
 class TestStudent(unittest.TestCase):
+
+    # DRY principle - setup test conditions at top of class for later
+    # Note use of camelCase
+    def setUp(self):
+        print("setUp")
+        # This creates an instance variable. All further references to
+        # student must be prepended with self
+        self.student = Student("John", "Doe")
+
+    def tearDown(self):
+        # Not actually used in this test suite, but printed to show when
+        # it runs in the testing procedure
+        print("tearDown")
+
     def test_full_name(self):
-        # create instance of the Student class in order to test it
-        student = Student("John", "Doe")
+        # print the currently tested function
+        print("test_full_name")
+        # student class instantiated in setUp function
         # test case - full name method displays correctly
-        self.assertEqual(student.full_name, "John Doe")
-
-    def test_alert_santa(self):
-        student = Student("John", "Doe")
-        student.alert_santa()
-
-        self.assertTrue(student.naughty_list)
+        self.assertEqual(self.student.full_name, "John Doe")
 
     def test_email_address(self):
-        # create instance of the Student class in order to test it
-        student = Student("John", "Doe")
+        print("test_email_address")
+        self.assertEqual(self.student.email, "john.doe@email.com")
 
-        self.assertEqual(student.email, "john.doe@email.com")
+    def test_alert_santa(self):
+        print("test_alert_santa")
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
 
 
 if __name__ == "__main__":
